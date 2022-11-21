90878c8ef5c1f6c9b261de033606982c301bd21b
slamtheliquid <47554339+slamtheliquid@users.noreply.github.com>
2022-11-21 21:26:01 +0200

    Delete lab1 directory

--- lab1/logger/Logger.java
+++
@@ -1,6 +0,0 @@
-package lab1.logger;
-
-public interface Logger {
-
-    void log(String log);
-}
--- lab1/message/Notificator.java
+++
@@ -1,8 +0,0 @@
-package lab1.message;
-
-import lab1.entity.User;
-
-public interface Notificator {
-
-    void notify(User user, String message);
-}
